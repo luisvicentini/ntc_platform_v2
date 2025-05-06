e467d14fb45ecf4d213424899e4f2b667ae1b7bc
LH <luishenrique.designer@gmail.com>
2025-05-06 18:16:36 -0300

    VENDAS-CHECKOUT: Criacao de links de checkout direto de vendas.

---
+++ app/(redirect)/(checkout)/garanta-sua-vaga-mc/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation'
+
+export default function Home() {
+  redirect('https://lastlink.com/p/C4D0F7E74/checkout-payment?&utm_source=manychat&utm_medium=checkout_direto&utm_term=lastlink&sck=manychat&versao_lp=v1&utm_campaign=manychat_checkout_direto_v1_manychat')
+}
+
